58c447dae19e4d83f1025935309c89942804aa8f
nicbongo <nicbongo@protonmail.com>
2020-02-26 18:19:43 -0400

    server up

--- data/api/server.js
+++ data/api/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const apirouter = require('')
-const middleware = require('')
+// const apirouter = require('')
+// const configureMiddleware = require('')
 
 const server = express();
 
+server.get('/', (req, res) => {
+  res.status(200).json({ api: WORKING })
+})
+
+module.exports = server
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-requestAnimationFrame('dotenv').config();
+require('dotenv').config();
 
 const server = require ('./data/api/server.js');
 
 const port = process.env.PORT || 5000;
-server.listen(port, () => coneole.log(`*** Server listening on port: ${port}`));
+server.listen(port, () => console.log(`*** Server listening on port: ${port}`));
